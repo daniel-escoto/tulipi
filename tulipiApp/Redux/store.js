e7c0184d17555f25f20e8f0e72c77a579049d6d0
Daniel <escotodanny62@gmail.com>
2021-07-25 12:10:44 -0700

    redux setup

---
+++ tulipiApp/Redux/store.js
@@ -0,0 +1,13 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunkMiddleware from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
+
+import cartItems from "./Reducers/cartItem";
+
+const reducers = combineReducers({
+  cartItems: cartItems,
+});
+
+const store = createStore(reducers, composeWithDevTools(thunkMiddleware));
+
+export default store;
