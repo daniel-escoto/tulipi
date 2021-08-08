e5a966b24dc9e9754859cafc0c8689325303e9a4
Daniel <escotodanny62@gmail.com>
2021-08-08 16:33:22 -0700

    backend changes

--- backend/app.js
+++ backend/app.js
@@ -43,6 +43,13 @@ mongoose
     console.log(err);
   });
 
+// Development
 app.listen(3000, () => {
   console.log("server is running http://localhost:3000");
 });
+
+// Production
+// const server = app.listen(process.env.PORT || 3000, function () {
+//   const port = server.address().port;
+//   console.log("Express is working on port " + port);
+// });
--- backend/helpers/jwt.js
+++ backend/helpers/jwt.js
@@ -14,8 +14,8 @@ function authJwt() {
       { url: /\/public\/upload(.*)/, methods: ["GET", "OPTIONS"] },
       { url: /\/api\/v1\/products(.*)/, methods: ["GET", "OPTIONS"] },
       { url: /\/api\/v1\/categories(.*)/, methods: ["GET", "OPTIONS"] },
+      { url: `${api}/users/`, methods: ["POST"] },
       `${api}/users/login`,
-      `${api}/users/register`,
     ],
   });
 }
